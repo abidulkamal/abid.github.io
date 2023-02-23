2cb32152a3f2a90d82380aa974a8eccfffc6dc3a
abidulkamal <64432937+abidulkamal@users.noreply.github.com>
2023-02-23 11:27:08 +0600

    removed unused containers

--- src/containers/skills/Skills.js
+++ src/containers/skills/Skills.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Skills.css";
-import SkillSection from "./SkillSection";
-import { Fade } from "react-reveal";
+/*import SkillSection from "./SkillSection";
+import { Fade } from "react-reveal";*/
 
 export default function Skills(props) {
-  const theme = props.theme;
+  /*const theme = props.theme;*/
   return (
     /*<div className="main" id="skills">
       <div className="skills-header-div">
